7bd974612dc312f1a7095e0647996a30a2fbceff
tkth99 <60248087+tkth99@users.noreply.github.com>
2020-02-16 10:04:59 -0500

    no longer need basemap library(uses geopandas)

--- app/visualization.py
+++ app/visualization.py
@@ -33,9 +33,8 @@ os.environ["PROJ_LIB"] = r'C:\Users\2k\Anaconda3\Library\share'
 
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure, show
-import numpy as np
-from numpy.random import rand
+import geopandas as gpd
+
 
 #conversion of string to float
 num = len(long)
@@ -55,20 +54,24 @@ for i in range(0, num):
     normNum = float((ratio[i]-ratioMin)/(ratioMax-ratioMin))
     norm.append(normNum)
 
-#zoom in to the state of virginia
-fig = plt.figure(figsize=(48,8))
-map = Basemap(projection='cyl', llcrnrlon = -84.5, llcrnrlat = 36.23, urcrnrlon = -75.15, urcrnrlat = 39.77, resolution = 'h', epsg = 2924)
-map.drawstates(linewidth = 0.25)
-map.drawcoastlines(linewidth = 0.25)
+#basemap library's version of plotting Virginia(other state borders included)
+#fig = plt.figure(figsize=(48,8))
+#map = Basemap(projection='cyl', llcrnrlon = -84.5, llcrnrlat = 36.23, urcrnrlon = -75.15, urcrnrlat = 39.77, resolution = 'h', epsg = 2924)
+#map.drawstates(linewidth = 0.25)
+#map.drawcoastlines(linewidth = 0.25)
 
-map.readshapefile('C:\\Users\\2k\\Documents\\GitHub\\mlh-hackathon-flask-starter\\app\\datasets\\us-zip-code-latitude-and-longitude\\us-zip-code-latitude-and-longitude', 'us-zip-code-latitude-and-longitude')
+#geopandas version of plotting the map of virginia
+usa = gpd.read_file('./states_21basic/states.shp')
+va = usa[usa.STATE_ABBR == 'VA'].plot(figsize = (48,8), facecolor = 'white', edgecolor = 'black', linewidth = 0.25)
+
+#map.readshapefile('C:\\Users\\2k\\Documents\\GitHub\\mlh-hackathon-flask-starter\\app\\datasets\\us-zip-code-latitude-and-longitude\\us-zip-code-latitude-and-longitude', 'us-zip-code-latitude-and-longitude')
 
 #plot coordinates onto a scatter plot 
 x = []
 y = []   
-x, y = map(long, lat)
+#x, y = map(long, lat)
 
-plt.scatter(x, y, c=norm, cmap= 'viridis', s = 20)
+plt.scatter(long, lat, c=norm, cmap= 'viridis', s = 20)
 plt.title("Virginia Property Sale-to-List Price Ratios per City (Purple = Low --> Blue = Medium/Average --> Green = High --> Yellow = Highest)")
 
 #label every plot point with a city name
